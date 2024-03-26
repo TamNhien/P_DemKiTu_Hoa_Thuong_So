826e09596563d104344b0e87c5cb7234b1776325
TamNhien <canhthien91@gmail.com>
2024-03-26 19:49:46 +0700

    Add project files.

---
+++ P_DemKiTu_Hoa_Thuong_So.py
@@ -0,0 +1,9 @@
+﻿string = input("Nhập chuỗi : ")
+upper_count = sum(1 for char in string if char.isupper())
+lower_count = sum(1 for char in string if char.islower())
+digit_count = sum(1 for char in string if char.isdigit())
+
+print("Số ký tự in hoa trong chuỗi là : ", upper_count)
+print("Số ký tự in thường trong chuỗi là : ", lower_count)
+print("Số ký tự số trong chuỗi là : ", digit_count)
+
